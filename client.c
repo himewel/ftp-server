bcfe4acaa6b05360c901bad35ae7b3b4616fb07f
Welbert Hime <welberthime@hotmail.com>
2019-04-26 15:55:33 -0300

    Adicionando arquivos das bibliotecas em comum

--- client.c
+++ client.c
@@ -1,14 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-//#include <arpa/inet.h>
-//#include <sys/types.h>
-#include <netinet/in.h>
-#include <sys/socket.h>
-
-#define TAM_BUFFER 500
-#define PORTNUM 2300
+#include "common.h"
 
 int main (void) {
   // Buffers de leitura e escrita
---
+++ common.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+//#include <arpa/inet.h>
+//#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+
+#define TAM_BUFFER 500
+#define PORTNUM 2300
