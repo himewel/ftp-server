d6f2da97c8f29f6255f5fd1c92924aa7f0f09ec5
Welbert Hime <welberthime@hotmail.com>
2019-04-27 22:18:36 -0300

    Implementando struct para manutenção de estadoe aplicando adaptação para o comando USER

--- server_func.c
+++ server_func.c
@@ -30,16 +30,18 @@ int decode_message (char *command) {
 }
 
 void strlwr (char s[]) {
-   int c = 0;
-   while (s[c] != '\0') {
-      if (s[c] >= 'A' && s[c] <= 'Z') {
-         s[c] = s[c] + 32;
-      }
-      c++;
-   }
+  // Tranforma caracteres da string em caixa baixa
+  int c = 0;
+  while (s[c] != '\0') {
+    if (s[c] >= 'A' && s[c] <= 'Z') {
+       s[c] = s[c] + 32;
+    }
+    c++;
+  }
 }
 
 int number_words(char *m) {
+  // Retorna número de palavras na string
   char *ptr = strtok(m, " ");
   int i;
   for (i = 0; ptr != NULL; i++) {
@@ -48,22 +50,57 @@ int number_words(char *m) {
   return i;
 }
 
-/* CONTROLE DE ACESSO */
+char **split_words(char *m) {
+  // Separa palavras em vetor
+  int i;
+  char *ptr = strtok(m, " ");
 
-char *func_user(char *message) {
-  int i = number_words(message);
-  if (i == 2) {
-    return "230 User logged in, proceed.";
-  } else {
-    return "501 Syntax error in parameters or arguments.";
+  char **res = (char**) malloc(MAX_ARGUMENTS*sizeof(char*));
+  for (i = 0; i < MAX_ARGUMENTS; i++) {
+    res[i] = (char *) malloc(STRING_SIZE*sizeof(char));
   }
+
+  for (i = 0; ptr != NULL && i < MAX_ARGUMENTS; i++) {
+    strcpy(res[i], ptr);
+    ptr = strtok(NULL, " ");
+  }
+  return res;
 }
 
-char *func_pass(char *message) {
+/* CONTROLE DE ACESSO */
+
+ConnectionStatus *func_user(ConnectionStatus *c, char *message) {
+  // Número de palavras dentro da mensagem
+  int i = number_words(message);
+
+  // Recebe mensagem decodificada em espaços, alocada itens do vetor
+  char **args = (char**) malloc(MAX_ARGUMENTS*sizeof(char*));
+  for (i = 0; i < MAX_ARGUMENTS; i++) {
+    args[i] = (char *) malloc(STRING_SIZE*sizeof(char));
+  }
+  args = split_words(message);
+
+  // Controle de sintaxe através do número de argumentos
+  if (i == 2) {
+    if (c->logged_on != 1) {
+      strcpy(c->user, args[1]);
+      c->logged_on = 1;
+      strcpy(c->return_message, "230 User logged in, proceed.");
+    } else {
+      strcpy(c->return_message, "503 You are already logged in.");
+    }
+  } else {
+    strcpy(c->return_message, "501 Syntax error in parameters or arguments.");
+  }
+  // Retorna novo estado da conexão
+  return c;
+}
+
+ConnectionStatus *func_pass(ConnectionStatus *c, char *message) {
   int i = number_words(message);
   if (i == 2) {
-    return "202 Command not implemented, superfluous at this site.";
+    return c;
   } else {
-    return "501 Syntax error in parameters or arguments.";
+    return c;
   }
 }
