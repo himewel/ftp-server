6df94f9ba1e7ada0a5fd3295fe179b24d6a47f21
Welbert Hime <welberthime@hotmail.com>
2019-04-29 18:19:17 -0300

    Retirando programa do cliente FTP

--- server_func.h
+++ server_func.h
@@ -1,4 +1,16 @@
-#include "common.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <dirent.h>
+
+#define PORTNUM 2300
+#define MAX_ARGUMENTS 5
+#define STRING_SIZE 500
 
 struct connection_status {
   char user[STRING_SIZE];
@@ -30,4 +42,4 @@ char *func_list(ConnectionStatus *c, char *message);
 char *func_pwd(ConnectionStatus *c,char *message);
 char *func_mkd(ConnectionStatus *c,char *message);
 char *func_rmd(ConnectionStatus *c, char *message);
-char *func_noop(ConnectionStatus *c, char *message); 
+char *func_noop(ConnectionStatus *c, char *message);
